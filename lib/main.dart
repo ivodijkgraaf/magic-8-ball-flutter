dd39e7c16ec260572fa8fb326ff70e003bab5c76
Ivo Dijkgraaf <github@ivodijkgraaf.com>
2020-12-08 15:04:40 +0100

    Init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,52 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Title'),
+          backgroundColor: Colors.indigo[900],
+        ),
+        body: MyHomePage(),
+        backgroundColor: Colors.indigo,
+      ),
+    ),
+  );
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int ballNumber = 1;
+
+  @override
+  void initState() {
+    showRandomBall();
+    super.initState();
+  }
+
+  void showRandomBall() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+      // print(ballNumber);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        child: Image.asset('images/ball$ballNumber.png'),
+        onPressed: () {
+          showRandomBall();
+        },
       ),
     );
+  }
+}
